3940a0cbd3cf2397c65fb8cbd5395a39c0c22434
GaelCruz <108242626+GaelCruz@users.noreply.github.com>
2025-01-25 13:50:25 -0600

    fixed padding of the upcoming event tile

--- src/components/EventTile/EventTile.jsx
+++ src/components/EventTile/EventTile.jsx
@@ -46,7 +46,6 @@ const EventTile = ({
               <span className="font-medium">
                 <svg
                   width="15"
-                  height="25"
                   viewBox="0 0 24 25"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
